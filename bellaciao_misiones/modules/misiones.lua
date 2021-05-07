0cd50b5ebabe86d9c1ab93d63fa2a0f9c50a7c8f
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-07 12:48:43 +0200

    Añadido ped para venta en su totalidad

--- bellaciao_misiones/modules/misiones.lua
+++ bellaciao_misiones/modules/misiones.lua
@@ -11,17 +11,25 @@ Citizen.CreateThread(function()
             playerCoords = GetEntityCoords(playerPed)
             inVeh = GetVehiclePedIsIn(playerPed)
             isInVeh = IsPedSittingInAnyVehicle(playerPed)
-            local vector = vector3(Config.Misiones.Ped.Position.x, Config.Misiones.Ped.Position.y, Config.Misiones.Ped.Position.z)
+            local vector = vector3(Config.Misiones.Peds.PedMisiones.Position.x, Config.Misiones.Peds.PedMisiones.Position.y, Config.Misiones.Peds.PedMisiones.Position.z)
+            local vector2 = vector3(Config.Misiones.Peds.PedVenta.Position.x, Config.Misiones.Peds.PedVenta.Position.y, Config.Misiones.Peds.PedVenta.Position.z)
             local distance = Vdist(GetEntityCoords(PlayerPedId()), vector.x, vector.y, vector.z)
+            local distance2 = Vdist(GetEntityCoords(PlayerPedId()), vector2.x, vector2.y, vector2.z)
 
             if distance <= 3 then
-                inMarker = "npc"
+                inMarker = "npc1"
                 time = 0
-                ESX.ShowFloatingHelpNotification(Config.Misiones.Traducciones.PedText, vector3(Config.Misiones.Ped.Position.x, Config.Misiones.Ped.Position.y, Config.Misiones.Ped.Position.z + 1.9))
+                ESX.ShowFloatingHelpNotification(Config.Misiones.Traducciones.PedText, vector3(Config.Misiones.Peds.PedMisiones.Position.x, Config.Misiones.Peds.PedMisiones.Position.y, Config.Misiones.Peds.PedMisiones.Position.z + 1.9))
+            elseif distance2 <= 3 then
+                inMarker = "npc2"
+                time = 0
+                ESX.ShowFloatingHelpNotification(Config.Misiones.Traducciones.PedVentaText, vector3(Config.Misiones.Peds.PedVenta.Position.x, Config.Misiones.Peds.PedVenta.Position.y, Config.Misiones.Peds.PedVenta.Position.z + 1.9))
             end
 
-            if inMarker == "npc" and IsControlJustPressed(0, 38) then
+            if inMarker == "npc1" and IsControlJustPressed(0, 38) then
                 OpenMissionMenu()
+            elseif inMarker == "npc2" and IsControlJustPressed(0, 38) then
+                TriggerServerEvent('nek_missions:server:helicopter:giveObject', 'ped')
             end
         end
         Citizen.Wait(time)
