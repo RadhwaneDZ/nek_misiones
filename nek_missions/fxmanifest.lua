023976118700a8d985cc3db56a03dad0f31f5792
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-17 16:01:29 +0200

    Update fxmanifest.lua

--- nek_missions/fxmanifest.lua
+++ nek_missions/fxmanifest.lua
@@ -3,5 +3,13 @@ game 'gta5'
 author 'Nekix'
 
 
-client_scripts {'config/*.lua', 'core/*.lua', 'modules/*.lua'}
-server_scripts {'config/*.lua', 'server/*.lua'}
+client_scripts {
+  'config/config.lua', 
+  'core/core.lua', 
+  'modules/*.lua'
+}
+
+server_scripts {
+  'config/config.lua', 
+  'server/main.lua'
+}
