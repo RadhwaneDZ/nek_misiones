010550a210c77dc31081e2a6dac7b6880e3879af
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-17 13:57:22 +0200

    Delete bellaciao_misiones directory

--- bellaciao_misiones/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version 'bodacius'
-game 'gta5'
-author 'Nekix'
-
-
-client_scripts {'config/*.lua', 'core/*.lua', 'modules/*.lua'}
-server_scripts {'config/*.lua', 'server/*.lua'}
--- bellaciao_misiones/sql/database.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `items` (name, label, weight) VALUES
-	('missions_rotorHeli', 'Rotor de Helicoptero (Droga camuflada)', 5)
-;
