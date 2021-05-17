e87f27c057c3bec8253144a07821d8ca077f6775
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-17 15:10:49 +0200

    Update core.lua

--- nek_missions/core/core.lua
+++ nek_missions/core/core.lua
@@ -27,6 +27,14 @@ function AddPeds()
             TaskStartScenarioInPlace(ped, "WORLD_HUMAN_SMOKING", 0, true)
 end
 
+ShowFloatingHelpNotification = function(msg, coords)
+	AddTextEntry('esxFloatingHelpNotification', msg)
+	SetFloatingHelpTextWorldPosition(1, coords)
+	SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
+	BeginTextCommandDisplayHelp('esxFloatingHelpNotification')
+	EndTextCommandDisplayHelp(2, false, false, -1)
+end
+
 function AddPed(x, y, z, w, model)
     --DeletePed(ped)
 
